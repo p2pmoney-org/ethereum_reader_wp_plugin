10578f29e4ded766c73917b2f12f10d43b80d86b
FredericBaud <fbaud@p2pventure.org>
2017-11-30 13:15:48 +0100

    controller and view to display account

--- ethereum_reader.php
+++ ethereum_reader.php
@@ -12,11 +12,19 @@ Text Domain: Ethereum
 defined( 'ABSPATH' ) or die( 'Nope, not accessing this' );
 
 require_once ( dirname(__FILE__) . '/./includes/global.inc');
-require_once ( plugin_dir_path(__FILE__) . '/./includes/wp_shortcodes.inc');
-require_once ( plugin_dir_path(__FILE__) . '/./includes/wp_activation.inc');
 
 require_once ( ethereum_reader_Globals::getEthereumReaderClientCodeDir().'/core/model/ethereumreaderserver.inc');
-require_once ( ethereum_reader_Globals::getEthereumReaderClientCodeDir().'/core/model/block.inc');
+require_once ( ethereum_reader_Globals::getEthereumReaderClientCodeDir().'/core/view/accountview.inc');
+
+
+require_once ( plugin_dir_path(__FILE__) . '/./includes/wp_shortcodes.inc');
+require_once ( plugin_dir_path(__FILE__) . '/./includes/wp_activation.inc');
+require_once ( plugin_dir_path(__FILE__) . '/./includes/wp_widgets.inc');
+
+
+//require_once ( ethereum_reader_Globals::getEthereumReaderClientCodeDir().'/core/model/blocks.inc');
+//require_once ( ethereum_reader_Globals::getEthereumReaderClientCodeDir().'/core/model/transactions.inc');
+//require_once ( ethereum_reader_Globals::getEthereumReaderClientCodeDir().'/core/model/accountsd.inc');
 
 ethereum_reader_Globals::logString('loading ethereum_reader plug-in');
 
@@ -32,5 +40,16 @@ register_uninstall_hook( __FILE__, 'ethereum_reader_plugin_uninstall');
 
 // shortcodes
 add_shortcode('ethereum_account_info', 'ethereum_reader_getAccountInfo');
+add_shortcode('ethereum_account_transactions_info', 'ethereum_reader_getAccountTransactionsInfo');
+
 add_shortcode('ethereum_last_blocks_info', 'ethereum_reader_getLastBlocksInfo');
+
+add_shortcode('ethereum_block_info', 'ethereum_reader_getBlockInfo');
+add_shortcode('ethereum_block_transactions_info', 'ethereum_reader_getBlockTransactionsInfo');
+
+
 add_shortcode('ethereum_last_transactions_info', 'ethereum_reader_getLastTransactionsInfo');
+add_shortcode('ethereum_transaction_info', 'ethereum_reader_getTransactionInfo');
+
+// widgets
+add_action( 'widgets_init', 'ethereum_reader_load_widgets' );
